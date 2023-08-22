4cab3598e75c7969ab6750c25674d8eb2ef4b6c8
darkfat123 <supakorn.yookack@gmail.com>
2023-08-22 11:36:24 +0700

    init

--- Walltrade/lib/pages/TradePageBuy.dart
+++ Walltrade/lib/pages/TradePageBuy.dart
@@ -78,7 +78,6 @@ class _TradePageBuyState extends State<TradePageBuy> {
     if (response.statusCode == 200) {
       print(body);
       setState(() {
-        
         result = response.body;
       });
     } else {
@@ -107,7 +106,6 @@ class _TradePageBuyState extends State<TradePageBuy> {
     if (response.statusCode == 200) {
       print(body);
       setState(() {
-        
         result = response.body;
       });
     } else {
@@ -116,7 +114,6 @@ class _TradePageBuyState extends State<TradePageBuy> {
       });
     }
   }
-
 
   Future<void> getBalance() async {
     var url = Uri.parse('${Constants.serverUrl}/getBalance');
